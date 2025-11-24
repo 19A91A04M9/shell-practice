655daca9be98ec2d5399b584b54a8e93d219aeaf
sai <ravisaikrishna1913@gmail.com>
2025-11-24 17:21:02 +0530

    installation

--- 06-installation.sh
+++ 06-installation.sh
@@ -3,24 +3,28 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "Error:: please run this script as root user"
+    echo "Error:: Please run this script as root user"
     exit 1
 fi 
 
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-        echo "mysql installed successfully"
-        exit 0
-else
-        echo "mysql installation failed"
+# VALIDATE FUNCTION
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo "$2 installation SUCCESS"
+    else
+        echo "$2 installation FAILED"
         exit 1
+    fi
 }
 
+# MYSQL
 dnf install mysql -y
+VALIDATE $? "MySQL"
 
-VALIDATE=$? "MYSQL"
-
+# NGINX
 dnf install nginx -y
-VALIDATE=$? "nginx"
+VALIDATE $? "Nginx"
+
+# MONGOSH
 dnf install mongodb-mongosh -y
-VALIDATE=$? "mongosh"
+VALIDATE $? "MongoSH"
