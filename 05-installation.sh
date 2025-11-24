bb78d185ba94662c7cdf84d41e9cd2669469afee
sai <ravisaikrishna1913@gmail.com>
2025-11-24 14:57:29 +0530

    installation

--- 05-installation.sh
+++ 05-installation.sh
@@ -7,4 +7,13 @@ if [ $USERID -ne 0 ]; then
     exit 1
 else
     echo "You are running this dcript as root user"
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]; then
+    echo "mysql installed successfully"
+    exit 0
+else
+    echo "mysql installation failed"
+    exit 1
 fi 
