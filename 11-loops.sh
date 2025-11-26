5db3c8359fe9892e39cf7030837376a09d2c8adf
sai <ravisaikrishna1913@gmail.com>
2025-11-26 11:28:32 +0530

    loops

---
+++ 11-loops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
+mkdir -p $LOGS_FOLDER
+echo " Script started Execution at : $(date) " | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Error:: Please run this script as root user $N"
+    exit 1
+fi 
+
+
+# VALIDATE FUNCTION
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo -e " $2 installation ... $G SUCCESS $N"
+    else
+        echo -e " $2 installation ... $R FAILED $N"
+        exit 1
+    fi
+}
+
+for package in $@ 
+do
+    echo "Package is: $package"
+done    
